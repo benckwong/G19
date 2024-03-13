9e145da0199bbfc164366471ff868c1a045b0a33
Yingshan <yingshanjiang6@gmail.com>
2024-03-13 23:50:35 +0000

    add

--- data_analysis.R
+++ data_analysis.R
@@ -2,7 +2,6 @@ library(ggplot2)
 
 product_data <- read.csv("Data_upload/Product.csv")
 
-# 创建散点图
 gg <- ggplot(product_data, aes(x = price, y = discount_rate)) +
   geom_point() +
   labs(title = "Price vs. Discount Rate",
